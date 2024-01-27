82b6521f0361164e1aabbe129ba8c096cff9abe1
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-28 00:02:40 +0530

    Added util function to set and get items from async storage

---
+++ app /constants/app_keys.ts
@@ -0,0 +1,3 @@
+export default {
+  savedNews: "saved_news",
+};
--- app /utils/helpers.ts
+++ app /utils/helpers.ts
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Dimensions, Platform, PixelRatio, Alert } from "react-native";
 import moment from "moment";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // print console logs, only in dev environment
 export const showConsole = (content: any) => {
@@ -28,6 +29,24 @@ export const showAlert = (payload: string) => {
 // convert date format
 export const convertDate = (payload: String) => {
   return moment(payload.toString()).format("DD MMM YYYY");
+};
+
+// set data in async storage
+export const setData = async (key: string, payload: string) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(payload));
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
+};
+
+// get data from async storage
+export const getData = async (key: string) => {
+  try {
+    return await AsyncStorage.getItem(key);
+  } catch (e) {
+    console.log(`Error: `);
+  }
 };
 
 export default {
